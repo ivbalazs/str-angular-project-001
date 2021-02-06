c91015936196bdb41fde4dec2efda25879320b91
ivbalazs <ivbalazs@gmail.com>
2021-02-06 14:03:56 +0100

    admin.component költöztetése data-editor.comp.-be

--- src/app/page/admin/admin.component.ts
+++ src/app/page/admin/admin.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Product } from 'src/app/model/product';
-import { ConfigService } from 'src/app/service/config.service';
-import { ITableCol } from 'src/app/service/config.service';
-import { ProductService } from 'src/app/service/product.service';
+// import { Observable } from 'rxjs';
+// import { Product } from 'src/app/model/product';
+// import { ConfigService } from 'src/app/service/config.service';
+// import { ITableCol } from 'src/app/service/config.service';
+// import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-admin',
@@ -12,31 +12,31 @@ import { ProductService } from 'src/app/service/product.service';
 })
 export class AdminComponent implements OnInit {
 
-  productList$: Observable<Product[]> = this.productService.getAll();
-  cols: ITableCol[] = this.config.tableCols;
+  // productList$: Observable<Product[]> = this.productService.getAll();
+  // cols: ITableCol[] = this.config.tableCols;
 
   //products: Product[] = this.productService.list;
 
   //currentProduct: Product = new Product();
 
   constructor(
-    private config: ConfigService,
-    private productService: ProductService
+    // private config: ConfigService,
+    // private productService: ProductService
   ) { }
 
   ngOnInit(): void { }
 
-  onUpdate(product: Product): void {
-    this.productService.update(product).subscribe(
-      updatedProduct => console.log(updatedProduct)
-    );
-  }
+  // onUpdate(product: Product): void {
+  //   this.productService.update(product).subscribe(
+  //     updatedProduct => console.log(updatedProduct)
+  //   );
+  // }
 
-  onDelete(product: Product): void {
-    this.productService.remove(product).subscribe(
-      () => console.log('deleted')
-    );
-  }
+  // onDelete(product: Product): void {
+  //   this.productService.remove(product).subscribe(
+  //     () => console.log('deleted')
+  //   );
+  // }
 
 }
   
