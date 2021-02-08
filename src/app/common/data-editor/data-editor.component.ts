7e92ee33e711cd0b534e214316d0cb5f066df0a8
kladya <konkla@gmail.com>
2021-02-08 22:03:13 +0100

    Filter pipe improved with drop-down menu and pagination added

--- src/app/common/data-editor/data-editor.component.ts
+++ src/app/common/data-editor/data-editor.component.ts
@@ -6,6 +6,10 @@ import { ITableCol } from 'src/app/service/config.service';
 import { ProductService } from 'src/app/service/product.service';
 import { map, tap } from 'rxjs/operators';
 
+interface IPageBtn {
+  page: number;
+}
+
 @Component({
   selector: 'app-data-editor',
   templateUrl: './data-editor.component.html',
@@ -17,6 +21,9 @@ export class DataEditorComponent implements OnInit {
   filterKey: string = 'name';
   filterKeys: string[] = Object.keys(new Product());
   productsNum: number;
+  pageSize: number = 10;
+  pageStart: number = 1;
+  currentPage: number = 1;
 
   @Input() phrase: string = '';
   @Output() updateClick: EventEmitter<Product> = new EventEmitter();
@@ -25,6 +32,22 @@ export class DataEditorComponent implements OnInit {
     map( (products: Product[]) => products.filter( product => product.catID === 1 ) ),
     tap( products => this.productsNum = products.length )
   );
+
+  get paginator(): IPageBtn[] {
+    const pages = [];
+    for (let i = 0; i < this.productsNum / this.pageSize && pages.length < 10; i++) {
+      const page = this.pageStart + i;
+      pages.push({page});
+    }
+    return pages;
+  }
+  get pageSliceStart(): number {
+    const index = this.currentPage - 1;
+    return index === 0 ? 0 : (index * this.pageSize);
+  }
+  get pageSliceEnd(): number {
+    return this.pageSliceStart + this.pageSize;
+  }
 
   constructor(
     private config: ConfigService,
@@ -46,4 +69,10 @@ export class DataEditorComponent implements OnInit {
     );
   }
 
+  onPaginate(ev: Event, btn: IPageBtn): void {
+    ev.preventDefault();
+    this.currentPage = btn.page;
+    this.pageStart = (btn.page - 5) < 1 ? 1 : (btn.page - 5);
+    console.log(btn);
+  }
 }
